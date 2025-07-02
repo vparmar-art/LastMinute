a582f359cbe49d0de5ee4f53138f2ebbd7fb52b7
Vikash Parmar <vikash.parmar.work@gmail.com>
2025-07-02 17:21:55 +0530

    fix api base url

--- ui/marketplace/lib/constants.dart
+++ ui/marketplace/lib/constants.dart
@@ -1,2 +1,2 @@
-const String baseUrl = "http://prod-lb-1625394403.us-east-1.elb.amazonaws.com/api/ecom";
-const String serverUrl = "http://prod-lb-1625394403.us-east-1.elb.amazonaws.com";
+const String baseUrl = "http://prod-lb-1092214212.us-east-1.elb.amazonaws.com/api/ecom";
+const String serverUrl = "http://prod-lb-1092214212.us-east-1.elb.amazonaws.com/";
