3b6e2166fc951e88cc9b14538441487d1e578b5a
Vikash Parmar <vikash.parmar.work@gmail.com>
2025-07-02 18:46:54 +0530

    update constants

--- ui/marketplace/lib/constants.dart
+++ ui/marketplace/lib/constants.dart
@@ -1,3 +1,3 @@
-const String baseUrl = "http://192.168.0.102:8000/api/ecom";
-const String usersBaseUrl = "http://192.168.0.102:8000/api/users";
+const String baseUrl = "http://prod-lb-1625394403.us-east-1.elb.amazonaws.com/api/ecom";
+const String usersBaseUrl = "http://prod-lb-1625394403.us-east-1.elb.amazonaws.com/api/users";
 const String mediaRootBaseUrl = "https://lastminute-media-root.s3.amazonaws.com";
